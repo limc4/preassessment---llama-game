0de412887cef878e7d6734197d29e3526dd50f5f
limc4 <limc4@middleton.school.nz>
2026-03-31 15:25:07 +1300

    v3 gravity and llama jump

--- llama_game_v3.py
+++ llama_game_v3.py
@@ -1,5 +1,5 @@
 """program for a llama game similar to the dinosaur game - v2
-initialising the llama and the ground
+adding gravity component and jumping
 created by Charlotte"""
 
 import pygame
@@ -24,16 +24,24 @@ white = (255, 255, 255)
 
 X_POSITION, Y_POSITION = 300, 400  # x and y position of the llama
 
-# surfaces to represent llama and ground
+# for gravity
+jumping = False
+
+Y_GRAVITY = 1 # can't be < 0.01 * JUMP_HEIGHT
+JUMP_HEIGHT = 20
+Y_VELOCITY = JUMP_HEIGHT
+
+# surfaces to represent llama and ground - 2 surfaces for jump/standing llama
 STANDING_SURFACE = pygame.transform.scale(pygame.image.load("images/Llama2.png"),
                                           (42, 58))
 JUMPING_SURFACE = pygame.transform.scale(pygame.image.load("images/Llama.png"),
                                          (42, 58))
 GROUND = pygame.image.load("images/ground.png")
 
-# set llama starting x,y
-llama_rect = STANDING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
 SCREEN.fill(white)  # white background
+
+# rectangle to control the position of llama
+llama_rect = STANDING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
 
 while True:  # let user quit
     for event in pygame.event.get():
@@ -41,9 +49,27 @@ while True:  # let user quit
             pygame.quit()
             sys.exit()
 
+    keys_pressed = pygame.key.get_pressed()
+
+    if keys_pressed[pygame.K_SPACE]:
+        jumping = True
+
     # set ground x,y
+    SCREEN.fill(white)
     SCREEN.blit(GROUND, (295, 340))
-    SCREEN.blit(JUMPING_SURFACE, llama_rect)
+
+    if jumping:  # jump with gravity
+        Y_POSITION -= Y_VELOCITY
+        Y_VELOCITY -= Y_GRAVITY
+        if Y_VELOCITY < -JUMP_HEIGHT:  # jump velocity increases until max then
+        # decrease until negative jump height
+            jumping = False
+            Y_VELOCITY = JUMP_HEIGHT
+        llama_rect = JUMPING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
+        SCREEN.blit(JUMPING_SURFACE, llama_rect)
+    else:
+        llama_rect = STANDING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
+        SCREEN.blit(STANDING_SURFACE, llama_rect)
 
     # position llama x,y
     llama_rect = STANDING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
